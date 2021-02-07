0f6adb6ac51773efae0f4d38d8c9d7e52b655a19
dajtmullaj <38816399+dajtmullaj@users.noreply.github.com>
2021-02-07 14:29:26 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import setuptools 
+
+setup(
+    name="", 
+    version="",
+    author="",
+    author_email="",
+    description="",
+    long_description=long_description,
+    long_description_content_type="",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
