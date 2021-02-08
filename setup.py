b6ff44e11e1c5df8e3fae87e2c1b10c8adf4d9b3
dajtmullaj <38816399+dajtmullaj@users.noreply.github.com>
2021-02-08 04:37:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup
 
-# read the contents of the README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name="chemplot", 
     version="0.1.0",
