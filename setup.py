18f06bad252ec57da960d5511a12752c37945f5d
dajtmullaj <38816399+dajtmullaj@users.noreply.github.com>
2021-02-08 03:13:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email="m.c.sorkun@differ.nl",
     description="A python library for chemical space visualization.",
     long_description=long_description,
-    long_description_content_type: text/x-rst,
+    long_description_content_type=text/x-rst,
     url="https://github.com/mcsorkun/ChemPlot",
     license="BSD",
     packages=["chemplot"],
