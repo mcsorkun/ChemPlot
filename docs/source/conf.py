8318e1d980e79da7e6662edb52d0a264c607d1de
dajtmullaj <38816399+dajtmullaj@users.noreply.github.com>
2021-03-13 16:39:35 +0100

    added logo to documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -73,3 +73,10 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Add logo
+html_logo = "ChemPlot_logo.jpg"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
