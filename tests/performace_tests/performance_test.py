7ea958dfbb8de61fd958d8119435e30fa2d7f376
dajtmullaj <d.mullaj@student.tue.nl>
2021-06-28 18:42:24 +0200

    added performance test

--- tests/performace_tests/performance_test.py
+++ tests/performace_tests/performance_test.py
@@ -2,22 +2,20 @@ from chemplot import Plotter
 import time
 import pandas as pd 
 import gc
-import csv
-from matplotlib import pyplot
 import datetime
 
 class PerformanceTest(object):
     
     def __init__(self):
-        self.data_LOGS = pd.read_csv(".\Data\\test_data\\R_1291_LOGS.csv") 
-        self.data_LOGP = pd.read_csv(".\Data\\test_data\\R_4200_LOGP.csv")
-        self.data_BACE = pd.read_csv(".\Data\\test_data\\R_1513_BACE.csv")
-        self.data_SAMPL = pd.read_csv(".\Data\\test_data\\R_642_SAMPL.csv") 
-        self.data_AQSOLDB = pd.read_csv(".\Data\\test_data\\R_9982_AQSOLDB.csv") 
-        self.data_BBBP = pd.read_csv(".\Data\\test_data\\C_2039_BBBP_2.csv") 
-        self.data_HIV_3 = pd.read_csv(".\Data\\test_data\\C_41127_HIV_3.csv") 
-        self.data_BACE_2 = pd.read_csv(".\Data\\test_data\\C_1513_BACE_2.csv")
-        self.data_CLINTOX_2 = pd.read_csv(".\Data\\test_data\\C_1478_CLINTOX_2.csv")
+        self.data_LOGS = pd.read_csv("..\\test_data\\R_1291_LOGS.csv") 
+        self.data_LOGP = pd.read_csv("..\\test_data\\R_4200_LOGP.csv")
+        self.data_BACE = pd.read_csv("..\\test_data\\R_1513_BACE.csv")
+        self.data_SAMPL = pd.read_csv("..\\test_data\\R_642_SAMPL.csv") 
+        self.data_AQSOLDB = pd.read_csv("..\\test_data\\R_9982_AQSOLDB.csv") 
+        self.data_BBBP = pd.read_csv("..\\test_data\\C_2039_BBBP_2.csv") 
+        self.data_HIV_3 = pd.read_csv("..\\test_data\\C_41127_HIV_3.csv") 
+        self.data_BACE_2 = pd.read_csv("..\\test_data\\C_1513_BACE_2.csv")
+        self.data_CLINTOX_2 = pd.read_csv("..\\test_data\\C_1478_CLINTOX_2.csv")
         
         self.df_perf_test = pd.DataFrame(columns=['name', 'data_samples', 'target_type', 'sym_type','execution_time_from_smiles','execution_time_pca','execution_time_tsne','execution_time_umap'])
 
@@ -38,59 +36,41 @@ class PerformanceTest(object):
         # For R data sets
         self.fun_performace_test(self.data_LOGS, 'R_1291_LOGS', '1291', 'R', 'tailored', 0)
         gc.collect()
-        pyplot.close('all')
         self.fun_performace_test(self.data_LOGS, 'R_1291_LOGS', '1291', 'R', 'structural', 1)
         gc.collect()
-        pyplot.close('all')
         self.fun_performace_test(self.data_LOGP, 'R_4200_LOGP', '4200', 'R', 'tailored', 2)
         gc.collect()
-        pyplot.close('all')
         self.fun_performace_test(self.data_LOGP, 'R_4200_LOGP', '4200', 'R', 'structural', 3)
         gc.collect()
-        pyplot.close('all')
         self.fun_performace_test(self.data_BACE, 'R_1513_BACE', '1513', 'R', 'tailored', 4)
         gc.collect()
-        pyplot.close('all')
         self.fun_performace_test(self.data_BACE, 'R_1513_BACE', '1513', 'R', 'structural', 5)
         gc.collect()
-        pyplot.close('all')
         self.fun_performace_test(self.data_SAMPL, 'R_642_SAMPL', '642', 'R', 'tailored', 6)
         gc.collect()
-        pyplot.close('all')
         self.fun_performace_test(self.data_SAMPL, 'R_642_SAMPL', '642', 'R', 'structural', 7)
         gc.collect()
-        pyplot.close('all')
         self.fun_performace_test(self.data_AQSOLDB, 'R_9982_AQSOLDB', '9982', 'R', 'tailored', 8)
         gc.collect()
-        pyplot.close('all')
         self.fun_performace_test(self.data_AQSOLDB, 'R_9982_AQSOLDB', '9982', 'R', 'structural', 9)
         gc.collect()
-        pyplot.close('all')
         # For C data sets
         self.fun_performace_test(self.data_BBBP, 'C_2039_BBBP_2', '2039', 'C', 'tailored', 10)
         gc.collect()
-        pyplot.close('all')
         self.fun_performace_test(self.data_BBBP, 'C_2039_BBBP_2', '2039', 'C', 'structural', 11)
         gc.collect()
-        pyplot.close('all')
         self.fun_performace_test(self.data_HIV_3, 'C_41127_HIV_3', '41127', 'C', 'tailored', 12)
         gc.collect()
-        pyplot.close('all')
         self.fun_performace_test(self.data_HIV_3, 'C_41127_HIV_3', '41127', 'C', 'structural', 13)
         gc.collect()
-        pyplot.close('all')
         self.fun_performace_test(self.data_BACE_2, 'C_1513_BACE_2', '1513', 'C', 'tailored', 14)
         gc.collect()
-        pyplot.close('all')
         self.fun_performace_test(self.data_BACE_2, 'C_1513_BACE_2', '1513', 'C', 'structural', 15)
         gc.collect()
-        pyplot.close('all')
         self.fun_performace_test(self.data_CLINTOX_2, 'C_1478_CLINTOX_2', '1478', 'C', 'tailored', 16)
         gc.collect()
-        pyplot.close('all')
         self.fun_performace_test(self.data_CLINTOX_2, 'C_1478_CLINTOX_2', '1478', 'C', 'structural', 17)
         gc.collect()
-        pyplot.close('all')
         
         self.df_perf_test.to_csv(file_name + '.csv')  
     
