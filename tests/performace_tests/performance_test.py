5987e7826649a506298aa0516916c2373503971e
dajtmullaj <d.mullaj@student.tue.nl>
2021-06-28 18:29:37 +0200

    added visual performance test

---
+++ tests/performace_tests/performance_test.py
@@ -0,0 +1,112 @@
+from chemplot import Plotter
+import time
+import pandas as pd 
+import gc
+import csv
+from matplotlib import pyplot
+import datetime
+
+class PerformanceTest(object):
+    
+    def __init__(self):
+        self.data_LOGS = pd.read_csv(".\Data\\test_data\\R_1291_LOGS.csv") 
+        self.data_LOGP = pd.read_csv(".\Data\\test_data\\R_4200_LOGP.csv")
+        self.data_BACE = pd.read_csv(".\Data\\test_data\\R_1513_BACE.csv")
+        self.data_SAMPL = pd.read_csv(".\Data\\test_data\\R_642_SAMPL.csv") 
+        self.data_AQSOLDB = pd.read_csv(".\Data\\test_data\\R_9982_AQSOLDB.csv") 
+        self.data_BBBP = pd.read_csv(".\Data\\test_data\\C_2039_BBBP_2.csv") 
+        self.data_HIV_3 = pd.read_csv(".\Data\\test_data\\C_41127_HIV_3.csv") 
+        self.data_BACE_2 = pd.read_csv(".\Data\\test_data\\C_1513_BACE_2.csv")
+        self.data_CLINTOX_2 = pd.read_csv(".\Data\\test_data\\C_1478_CLINTOX_2.csv")
+        
+        self.df_perf_test = pd.DataFrame(columns=['name', 'data_samples', 'target_type', 'sym_type','execution_time_from_smiles','execution_time_pca','execution_time_tsne','execution_time_umap'])
+
+        
+    def fun_performace_test(self, data, name, data_samples, target_type, sim_type, index):
+        t0 = time.time()
+        cp = Plotter.from_smiles(data["smiles"], target=data["target"], target_type=target_type, sim_type=sim_type)
+        t1 = time.time()
+        cp.pca()
+        t2 = time.time()
+        cp.tsne()
+        t3 = time.time()
+        cp.umap()
+        t4 = time.time()
+        self.df_perf_test.loc[index] = [name, data_samples, target_type, sim_type, "%.2f" % (t1-t0), "%.2f" % (t2-t1), "%.2f" % (t3-t2), "%.2f" % (t4-t3)]
+        
+    def run(self, file_name): 
+        # For R data sets
+        self.fun_performace_test(self.data_LOGS, 'R_1291_LOGS', '1291', 'R', 'tailored', 0)
+        gc.collect()
+        pyplot.close('all')
+        self.fun_performace_test(self.data_LOGS, 'R_1291_LOGS', '1291', 'R', 'structural', 1)
+        gc.collect()
+        pyplot.close('all')
+        self.fun_performace_test(self.data_LOGP, 'R_4200_LOGP', '4200', 'R', 'tailored', 2)
+        gc.collect()
+        pyplot.close('all')
+        self.fun_performace_test(self.data_LOGP, 'R_4200_LOGP', '4200', 'R', 'structural', 3)
+        gc.collect()
+        pyplot.close('all')
+        self.fun_performace_test(self.data_BACE, 'R_1513_BACE', '1513', 'R', 'tailored', 4)
+        gc.collect()
+        pyplot.close('all')
+        self.fun_performace_test(self.data_BACE, 'R_1513_BACE', '1513', 'R', 'structural', 5)
+        gc.collect()
+        pyplot.close('all')
+        self.fun_performace_test(self.data_SAMPL, 'R_642_SAMPL', '642', 'R', 'tailored', 6)
+        gc.collect()
+        pyplot.close('all')
+        self.fun_performace_test(self.data_SAMPL, 'R_642_SAMPL', '642', 'R', 'structural', 7)
+        gc.collect()
+        pyplot.close('all')
+        self.fun_performace_test(self.data_AQSOLDB, 'R_9982_AQSOLDB', '9982', 'R', 'tailored', 8)
+        gc.collect()
+        pyplot.close('all')
+        self.fun_performace_test(self.data_AQSOLDB, 'R_9982_AQSOLDB', '9982', 'R', 'structural', 9)
+        gc.collect()
+        pyplot.close('all')
+        # For C data sets
+        self.fun_performace_test(self.data_BBBP, 'C_2039_BBBP_2', '2039', 'C', 'tailored', 10)
+        gc.collect()
+        pyplot.close('all')
+        self.fun_performace_test(self.data_BBBP, 'C_2039_BBBP_2', '2039', 'C', 'structural', 11)
+        gc.collect()
+        pyplot.close('all')
+        self.fun_performace_test(self.data_HIV_3, 'C_41127_HIV_3', '41127', 'C', 'tailored', 12)
+        gc.collect()
+        pyplot.close('all')
+        self.fun_performace_test(self.data_HIV_3, 'C_41127_HIV_3', '41127', 'C', 'structural', 13)
+        gc.collect()
+        pyplot.close('all')
+        self.fun_performace_test(self.data_BACE_2, 'C_1513_BACE_2', '1513', 'C', 'tailored', 14)
+        gc.collect()
+        pyplot.close('all')
+        self.fun_performace_test(self.data_BACE_2, 'C_1513_BACE_2', '1513', 'C', 'structural', 15)
+        gc.collect()
+        pyplot.close('all')
+        self.fun_performace_test(self.data_CLINTOX_2, 'C_1478_CLINTOX_2', '1478', 'C', 'tailored', 16)
+        gc.collect()
+        pyplot.close('all')
+        self.fun_performace_test(self.data_CLINTOX_2, 'C_1478_CLINTOX_2', '1478', 'C', 'structural', 17)
+        gc.collect()
+        pyplot.close('all')
+        
+        self.df_perf_test.to_csv(file_name + '.csv')  
+    
+    
+if __name__ == '__main__':
+    date = datetime.datetime.now()
+    date_test =  str(date.day) + "_" + str(date.month) + "_" + str(date.year)
+    file_name = "performance_test_" + date_test
+    
+    # Create metadata
+    py_version = "Python version: 3.7.9\n"
+    cpu_version = "Processor version: Intel(R) Core(TM) i7-7700HQ CPU @ 2.80GHz  2.81GHz\n"
+    os_version = "Operating System version: Windows 10 Enterprise"
+    metadata = open(file_name + '_metadata.txt',"w") 
+    metadata.writelines([py_version, cpu_version, os_version])  
+    metadata.close()
+    
+    # Run performance tests
+    PerformanceTest().run(file_name)
---
+++ tests/performace_tests/visual_plots_test.py
@@ -0,0 +1,93 @@
+from chemplot import Plotter
+import pandas as pd 
+from matplotlib import pyplot
+from matplotlib.backends.backend_pdf import PdfPages
+
+class PlotsTest(object):
+    
+    def __init__(self):
+        self.output = PdfPages('Visual_Plots_Test.pdf')
+    
+    def run(self, reg_data_1, reg_data_2, class_data_1, class_data_2):
+        self.plot_regression_data(reg_data_1[0], reg_data_1[1])
+        self.plot_regression_data(reg_data_2[0], reg_data_2[1])
+        self.plot_classification_data(class_data_1[0], class_data_1[1])
+        self.plot_classification_data(class_data_2[0], class_data_2[1])
+        
+        self.output.close()
+
+
+    def plot_regression_data(self, reg_data, name_data):
+        # Create plotter objects for tailored and structural similarity
+        cp_tailored = Plotter.from_smiles(reg_data["smiles"], target=reg_data["target"], target_type="R", sim_type='tailored')
+        cp_structural = Plotter.from_smiles(reg_data["smiles"], target=reg_data["target"], target_type="R", sim_type='structural')      
+        
+        self.generate_plots(cp_tailored, name_data + ' Regression Tailored -')
+        self.generate_plots(cp_structural, name_data + ' Regression Structural -')
+        
+    
+    def plot_classification_data(self, class_data, name_data):
+        # Create plotter objects for tailored and structural similarity
+        cp_tailored = Plotter.from_smiles(class_data["smiles"], target=class_data["target"], target_type="C", sim_type='tailored')
+        cp_structural = Plotter.from_smiles(class_data["smiles"], target=class_data["target"], target_type="C", sim_type='structural')      
+    
+        self.generate_plots(cp_tailored, name_data + ' Classification Tailored -')
+        self.generate_plots(cp_structural, name_data + ' Classification Structural -')
+        
+        
+    def generate_plots(self, cp, name):
+        size = 20
+        cp.pca()
+        # Get scatter plots PCA
+        plot_pca_scatter = cp.visualize_plot(kind="scatter", size=size, remove_outliers=False, is_colored=True, colorbar=False)
+        plot_pca_scatter.set_title(name + " pca scatter plot",fontsize=size*2)
+        self.output.savefig(plot_pca_scatter.figure)
+        # Get hex plots PCA
+        plot_pca_hex = cp.visualize_plot(kind="hex", size=size, remove_outliers=False, is_colored=True, colorbar=False)
+        plot_pca_hex.set_title(name + " pca hex plot",fontsize=size*2)
+        self.output.savefig(plot_pca_hex.figure)
+        # Get kde plots PCA
+        plot_pca_kde = cp.visualize_plot(kind="kde", size=size, remove_outliers=False, is_colored=True, colorbar=False)
+        plot_pca_kde.set_title(name + " pca kde plot",fontsize=size*2)
+        self.output.savefig(plot_pca_kde.figure)
+        
+        cp.tsne(perplexity=None, random_state=None, pca=False)
+        # Get scatter plots t-SNE
+        plot_tsne_scatter = cp.visualize_plot(kind="scatter", size=20, remove_outliers=False, is_colored=True, colorbar=False)
+        plot_tsne_scatter.set_title(name + " tsne scatter plot",fontsize=size*2)
+        self.output.savefig(plot_tsne_scatter.figure)
+        # Get hex plots t-SNE
+        plot_tsne_hex = cp.visualize_plot(kind="hex", size=20, remove_outliers=False, is_colored=True, colorbar=False)
+        plot_tsne_hex.set_title(name + " tsne hex plot",fontsize=size*2)
+        self.output.savefig(plot_tsne_hex.figure)
+        # Get kde plots t-SNE
+        plot_tsne_kde = cp.visualize_plot(kind="kde", size=20, remove_outliers=False, is_colored=True, colorbar=False)
+        plot_tsne_kde.set_title(name + " tsne kde plot",fontsize=size*2)
+        self.output.savefig(plot_tsne_kde.figure)
+
+        cp.umap(n_neighbors=None, min_dist=None, random_state=None)
+        # Get scatter plots UMAP
+        plot_umap_scatter = cp.visualize_plot(kind="scatter", size=20, remove_outliers=False, is_colored=True, colorbar=False)
+        plot_umap_scatter.set_title(name + " umap scatter plot",fontsize=size*2)
+        self.output.savefig(plot_umap_scatter.figure)
+        # Get hex plots UMAP
+        plot_umap_hex = cp.visualize_plot(kind="hex", size=20, remove_outliers=False, is_colored=True, colorbar=False)
+        plot_umap_hex.set_title(name + " umap hex plot",fontsize=size*2)
+        self.output.savefig(plot_umap_hex.figure)
+        # Get kde plots UMAP
+        plot_umap_kde = cp.visualize_plot(kind="kde", size=20, remove_outliers=False, is_colored=True, colorbar=False)
+        plot_umap_kde.set_title(name + " umap kde plot",fontsize=size*2)
+        self.output.savefig(plot_umap_kde.figure)
+        
+        pyplot.close('all')
+        
+        
+        
+if __name__ == '__main__':
+    data_LOGS = pd.read_csv("..\\test_data\\R_1291_LOGS.csv") 
+    data_BACE = pd.read_csv("..\\test_data\\R_1513_BACE.csv")
+    
+    data_BBBP = pd.read_csv("..\\test_data\\C_2039_BBBP_2.csv")
+    data_BACE_2 = pd.read_csv("..\\test_data\\C_1513_BACE_2.csv")
+    
+    PlotsTest().run([data_LOGS, 'LOGS'], [data_BACE, 'BACE'], [data_BBBP, 'BBBP'], [data_BACE_2, 'BACE_2'])
