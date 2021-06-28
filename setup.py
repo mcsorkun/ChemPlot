32059c32cb46e45d135ce58cf01b6bb53c9167b0
dajtmullaj <d.mullaj@student.tue.nl>
2021-06-28 03:16:07 +0200

    updated versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="chemplot", 
-    version="1.0.1",
+    version="1.0.2",
     author="Murat Cihan Sorkun",
     author_email="mcsorkun@gmail.com",
     description="A python library for chemical space visualization.",
