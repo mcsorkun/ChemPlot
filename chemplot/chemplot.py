329585bb3bd2cd6751abd38f6ddbc8d672e16358
dajtmullaj <d.mullaj@student.tue.nl>
2022-01-14 15:36:13 +0100

    Sanitazed input checks for from_smiles

--- chemplot/chemplot.py
+++ chemplot/chemplot.py
@@ -17,6 +17,7 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
+from pandas.api.types import is_numeric_dtype
 from rdkit.Chem import Draw
 from bokeh.plotting import figure
 from bokeh.transform import transform, factor_cmap
@@ -78,6 +79,8 @@ class Plotter(object):
         # Error handeling sym_type
         if sim_type not in self._sim_types:
             if len(target) > 0:
+                if len(target) != len(encoding_list):
+                    raise Exception("Target length must match the instances of molecules")
                 self.__sim_type = 'tailored' 
                 print('sim_type indicates the similarity type by which the plots are constructed.\n' +
                       'The supported similarity types are structural and tailored.\n' +
@@ -97,7 +100,8 @@ class Plotter(object):
         if len(target) > 0:
             df_target = pd.DataFrame(data=target)
             unique_targets_ratio = 1.*df_target.iloc[:, 0].nunique()/df_target.iloc[:, 0].count() < 0.05
-            if target_type == 'R' and unique_targets_ratio:
+            numeric_target = is_numeric_dtype(df_target.dtypes[0])
+            if target_type == 'R' and (unique_targets_ratio or not numeric_target):
                 print('Input received is \'R\' for target values that seem not continuous.')
             if target_type not in self._target_types:
                 if unique_targets_ratio:
@@ -124,8 +128,8 @@ class Plotter(object):
         elif self.__sim_type == "structural":
             self.__mols, self.__df_descriptors, self.__target = get_fingerprints(encoding_list,target,2,2048)
             
-        if not self.__mols or self.__df_descriptors.empty:
-                raise Exception("Plotter object cannot be instantiated for given molecules")
+        if len(self.__mols) < 2 or len(self.__df_descriptors.columns) < 2:
+            raise Exception("Plotter object cannot be instantiated for given molecules")
                 
         self.__df_2_components = None
         self.__plot_title = None
@@ -428,10 +432,10 @@ class Plotter(object):
                 if is_colored:
                     df_data = df_data.assign(target=self.__target)
                     hue = 'target'
+                    palette = 'deep'
                     if self.__target_type == "R":
                         palette = sns.color_palette("inferno", as_cmap=True)
-                    else:
-                        palette = 'deep'
+                        
         
         # Remove outliers (using Z-score)
         if remove_outliers:
@@ -444,6 +448,7 @@ class Plotter(object):
         
         # Create a plot based on the reduced components 
         if kind == "scatter":
+            print(type(palette))
             plot = sns.scatterplot(x=x, y=y, hue=hue, hue_order=hue_order, palette=palette, data=df_data, s=size*3)
             plot.set_label("scatter")
             axis = plot
