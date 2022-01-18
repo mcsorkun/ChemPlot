f429ebed5142600c4565569a2188a50ca8ddbbd6
Dajt Mullaj <38816399+dajtmullaj@users.noreply.github.com>
2022-01-18 21:39:28 +0100

    Updated version docs

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Dajt Mullaj, Murat Cihan Sorkun'
 author = 'Dajt Mullaj, Murat Cihan Sorkun'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '1.2.0'
 
 
 # -- General configuration ---------------------------------------------------
